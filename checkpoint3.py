afee086f05c3fae4893ffd9e12075f10c439952c
Josu Cabrero <150235461+josucabrero@users.noreply.github.com>
2024-02-06 12:01:31 +0100

    Add files via upload

---
+++ checkpoint3.py
@@ -0,0 +1,49 @@
+#Exercise 1
+
+my_string = 'Hello world'
+
+my_number = 16
+
+my_list = [1, 2, 3, 4]
+
+my_boolean = True
+
+#Exercise 2
+
+first_three_letters = my_string[:3]
+
+#Exercise 3
+
+first_element = my_list [0]
+
+#Exercise 4
+
+new_number = my_number + 10
+
+#Exercise 5
+
+last_element = my_list [-1]
+
+#Exercise 6 
+
+names = 'harry,alex,susie,jared,gail,conner'
+
+new_names = names.split(',')
+
+#Exercise 7
+
+first_space_index = my_string.index(" ")
+
+first_word = my_string[:first_space_index]
+
+uppercase_first_word = first_word.upper()
+
+new_string = uppercase_first_word + my_string[first_space_index:]
+
+#Exercise 8
+
+sentence = f'The winning number is: {my_number}'
+
+#Exercise 9
+
+print(my_string)
